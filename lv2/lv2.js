201e5cb6560187c6fccaddd01509e5a017021360
Kyano11 <55793746+Kyano11@users.noreply.github.com>
2019-11-22 01:49:57 +0900

    a
    a

--- lv2/lv2.js
+++ lv2/lv2.js
@@ -1,3 +1,3 @@
-document.getElementById("contents1").onclick = function() {
-    document.getElementById("text1").innerHTML = "Hello JS";
+document.getElementById("contents").onclick = function() {
+    document.getElementById("text").innerHTML = "Hello JS";
 };
