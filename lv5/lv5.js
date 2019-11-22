5fd76efc070ec19ec0fcb109d15d81b7d4af59ce
Kyano11 <55793746+Kyano11@users.noreply.github.com>
2019-11-23 02:28:22 +0900

    lv5改訂

--- lv5/lv5.js
+++ lv5/lv5.js
@@ -7,3 +7,19 @@ function showClock1() {
     document.getElementById("RealtimeClockArea").innerHTML = msg;
 }
 setInterval('showClock1()',1000);
+
+function set2fig(num) {
+    var ret;
+    if( num < 10 ) { ret = "0" + num; }
+    else { ret = num; }
+    return ret;
+ }
+ function showClock2() {
+    var nowTime = new Date();
+    var nowHour = set2fig( nowTime.getHours() );
+    var nowMin  = set2fig( nowTime.getMinutes() );
+    var nowSec  = set2fig( nowTime.getSeconds() );
+    var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+    document.getElementById("RealtimeClockArea2").innerHTML = msg;
+ }
+ setInterval('showClock2()',1000);
