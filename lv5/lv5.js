484bbe41c05f889c5e7da1e31844d9793ccd7f99
Kyano11 <55793746+Kyano11@users.noreply.github.com>
2019-11-23 02:20:07 +0900

    Update lv5.js

--- lv5/lv5.js
+++ lv5/lv5.js
@@ -3,4 +3,5 @@ var nowHour = nowTime.getHours();
 var nowMin  = nowTime.getMinutes();
 var nowSec  = nowTime.getSeconds();
 var msg     = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + "です。";
+setInterval('showClock1()',1000);
 document.getElementById("RealtimeClockArea").innerHTML = msg;
